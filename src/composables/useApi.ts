1a09fc509e33a0a888604f363359b45c4be59c56
Borys Tyminski <tyminski.borys@gmail.com>
2024-02-12 16:48:08 +0100

    fix useApi composable

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -2,25 +2,18 @@ import useRequest from './helpers/useRequest';
 import useResponse from './helpers/useResponse';
 import {AxiosRequestConfig} from "axios";
 
-export default (
-  requestConstructor: AxiosRequestConfig,
-  requestValues: Record<string, Record<string, string | number | null>> = {}
-): Promise<{ status: string; isSuccess: boolean; data: unknown; errors: object; } | any> | any => {
+export default (requestConstructor: AxiosRequestConfig, requestValues: any = {}):
+    Promise<{ status: string; isSuccess: boolean; data: unknown; errors: object; } | any> | any => {
 
     let response;
     let valuesKeys  = requestValues ? Object.keys(requestValues) : [];
-    let requestValuesBody
 
     // Shortcut for POST : If no 'params','query','body' keys in requestValues object, so it is 'body' object itself.
-    if (valuesKeys.length && !valuesKeys.some((key: string) => ['path', 'params', 'query', 'body'].includes(key))) {
-        const body = { ...requestValues };
-        requestValuesBody = {
-            body
-        }
+    if (valuesKeys.length && valuesKeys.some((key: string) => ['path', 'params', 'query', 'body'].includes(key))) {
+        requestValues.body = { ...requestValues };
     }
 
-    const request = useRequest(requestConstructor, requestValuesBody);
-    console.log('►►►', request.method.toUpperCase(), request.path, requestConstructor, requestValuesBody);
+    const request = useRequest(requestConstructor, requestValues);
 
     if (request)
         response = useResponse({ method: request.method, path: request.path, exec: request.exec });
